0cf9b4adcd850c28ae265e92314b05589c610e58
umbertop <umberto.palazzini@gmail.com>
2017-11-26 21:20:59 +0100

    feat(download): add download function
    Added download function and transfer progress to retrieve that download.

--- src/main/java/com/umbertopalazzini/s3zilla/model/S3Client.java
+++ src/main/java/com/umbertopalazzini/s3zilla/model/S3Client.java
@@ -3,10 +3,12 @@ package com.umbertopalazzini.s3zilla.model;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
+import com.amazonaws.services.s3.transfer.Download;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.TransferProgress;
 
-import java.util.ArrayList;
+import java.io.File;
 import java.util.List;
 
 public class S3Client {
@@ -14,11 +16,31 @@ public class S3Client {
 
     private AmazonS3 amazonS3Client;
     private TransferManager transferManager;
+    private TransferProgress transferProgress;
 
+    /**
+     * Returns the current transfer (upload/download) progress.
+     * @return
+     */
+    public TransferProgress getTransferProgress(){
+        return this.transferProgress;
+    }
+
+    /**
+     * Lists the buckets.
+     *
+     * @return
+     */
     public List<Bucket> listBuckets() {
         return amazonS3Client.listBuckets();
     }
 
+    /**
+     * List the folders in a bucket.
+     *
+     * @param bucket
+     * @return
+     */
     public List<String> listFolders(Bucket bucket) {
         ListObjectsV2Request listRequest;
         ListObjectsV2Result listResult;
@@ -29,11 +51,18 @@ public class S3Client {
         return listResult.getCommonPrefixes();
     }
 
-    public List<S3ObjectSummary> listFiles(Bucket bucket, String folder){
+    /**
+     * Lists the files (keys) in a bucket and in an optional speficied folder.
+     *
+     * @param bucket
+     * @param folder
+     * @return
+     */
+    public List<S3ObjectSummary> listFiles(Bucket bucket, String folder) {
         ListObjectsV2Request listRequest;
         ListObjectsV2Result listResult;
 
-        if(folder == null || folder.equals("")){
+        if (folder == null || folder.equals("")) {
             listRequest = new ListObjectsV2Request().withBucketName(bucket.getName()).withDelimiter("/");
             listResult = amazonS3Client.listObjectsV2(listRequest);
         } else {
@@ -44,6 +73,36 @@ public class S3Client {
         return listResult.getObjectSummaries();
     }
 
+    public void download(S3ObjectSummary s3Object) {
+        Download download;
+        File downloadFile;
+
+        downloadFile = new File("nome file");
+        // String filename = s3Object.getKey();
+
+        /*
+        filename = !filename.contains("/")
+                ? filename
+                : filename.substring(filename.lastIndexOf("/") + 1);
+        */
+
+        download = transferManager.download(s3Object.getBucketName(), s3Object.getKey(), downloadFile);
+
+        // TODO: manage thread
+        do{
+            try{
+                Thread.sleep(100);
+            } catch (InterruptedException e){
+                return;
+            }
+
+            transferProgress = download.getProgress();
+        } while(!download.isDone());
+    }
+
+    /**
+     * Initializes the s3 client and the transfer manager in order to track downloads/uploads progress.
+     */
     private void initialize() {
         amazonS3Client = AmazonS3ClientBuilder.defaultClient();
         transferManager = TransferManagerBuilder.defaultTransferManager();
