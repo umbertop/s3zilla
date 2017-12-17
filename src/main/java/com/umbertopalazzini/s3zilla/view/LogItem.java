09c8d94c074a472a44dcaa64800644ed358125b3
umbertop <umberto.palazzini@gmail.com>
2017-12-17 22:16:33 +0100

    feat(logitem): add view class for log table
    Added view class LogItem in order to properly format the row in the log table.

---
+++ src/main/java/com/umbertopalazzini/s3zilla/view/LogItem.java
@@ -0,0 +1,54 @@
+package com.umbertopalazzini.s3zilla.view;
+
+import com.amazonaws.services.s3.transfer.Download;
+import com.amazonaws.services.s3.transfer.Transfer;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+/**
+ * View purpose class in order to well organize data in the logTable.
+ * I need:
+ * - local file name
+ * - remote file name (I retrieve it from the transfer)
+ * - progress (it's simply a progress bar)
+ * - size (I retrieve it from the transfer)
+ * - status
+ */
+public class LogItem {
+    private ProgressBar progress;
+    private Transfer transfer;
+    private String localFile;
+    private Label status;
+
+    public LogItem(String localFile, ProgressBar progressBar, Transfer transfer, Label status) {
+        this.localFile = localFile;
+        this.progress = progressBar;
+        this.transfer = transfer;
+        this.status = status;
+    }
+
+    public String getLocalFile() {
+        return this.localFile;
+    }
+
+    public String getRemoteFile() {
+        if (transfer instanceof Download) {
+            return ((Download) this.transfer).getKey();
+        } else {
+            return localFile;
+        }
+    }
+
+    public ProgressBar getProgress() {
+        return this.progress;
+    }
+
+    public long getSize(){
+        return transfer.getProgress().getTotalBytesToTransfer();
+    }
+
+    public Label getStatus(){
+        return this.status;
+    }
+}
