0a0c8538f8fcad3305bf3e5ee5a02088c8b80aa1
umbertop <umberto.palazzini@gmail.com>
2017-12-19 22:37:51 +0100

    chore(constants): class for consts
    Added a class to save all the constants that will be used in the project.

---
+++ src/main/java/com/umbertopalazzini/s3zilla/utility/Consts.java
@@ -0,0 +1,7 @@
+package com.umbertopalazzini.s3zilla.utility;
+
+import java.io.File;
+
+public class Consts {
+    public static final String DOWNLOAD_PATH = System.getProperty("user.home") + File.separator + "S3Zilla" + File.separator;
+}
