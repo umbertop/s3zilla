9faa652016034aa435c89ce3885fb0d2994caa97
umbertop <umberto.palazzini@gmail.com>
2017-12-20 20:55:34 +0100

    chore(constants): add few more constants
    Added more constants for kB, MB and GB.

--- src/main/java/com/umbertopalazzini/s3zilla/utility/Consts.java
+++ src/main/java/com/umbertopalazzini/s3zilla/utility/Consts.java
@@ -6,8 +6,9 @@ public class Consts {
     public static final String DOWNLOAD_PATH = System.getProperty("user.home") + File.separator + "S3Zilla" + File.separator;
 
     public static final long BYTE = 1024L;
-    public static final long MB = 1024L * 1024L;
-    public static final long GB = 1024L * 1024L * 1024L;
+    public static final long KB = 1024L * 1024L;
+    public static final long MB = 1024L * 1024L * 1024L;
+    public static final long GB = 1024L * 1024L * 1024L * 1024L;
 
     public static final String ITALIAN = "locales/locale_it_IT";
     public static final String ENGLISH = "locales/locale_en_GB";
