0893c034938af2794bedf9cdad2c2101bdb2cf32
umbertop <umberto.palazzini@gmail.com>
2017-12-20 20:06:40 +0100

    chore(constants): add more consts
    Added more constants.

--- src/main/java/com/umbertopalazzini/s3zilla/utility/Consts.java
+++ src/main/java/com/umbertopalazzini/s3zilla/utility/Consts.java
@@ -4,4 +4,8 @@ import java.io.File;
 
 public class Consts {
     public static final String DOWNLOAD_PATH = System.getProperty("user.home") + File.separator + "S3Zilla" + File.separator;
+    public static final long BYTE = 1024L;
+    public static final long MB = 1024L * 1024L;
+    public static final long GB = 1024L * 1024L * 1024L;
+
 }
