c729712a69355cbf06ca03a8e3d2dbaed35f9c68
umbertop <umberto.palazzini@gmail.com>
2017-12-20 20:11:18 +0100

    chore(constants): added more constants for locales
    Added a couple of constants for the names of the locales.

--- src/main/java/com/umbertopalazzini/s3zilla/utility/Consts.java
+++ src/main/java/com/umbertopalazzini/s3zilla/utility/Consts.java
@@ -4,8 +4,11 @@ import java.io.File;
 
 public class Consts {
     public static final String DOWNLOAD_PATH = System.getProperty("user.home") + File.separator + "S3Zilla" + File.separator;
+
     public static final long BYTE = 1024L;
     public static final long MB = 1024L * 1024L;
     public static final long GB = 1024L * 1024L * 1024L;
 
+    public static final String ITALIAN = "locales/locale_it_IT";
+    public static final String ENGLISH = "locales/locale_en_GB";
 }
