845404bbd66f90b5696ece65602d5374a4947af4
umbertop <umberto.palazzini@gmail.com>
2017-12-22 20:06:04 +0100

    chore(consts): add text consts
    Added few more constants in order to give the text to the labels (see TransferTask).

--- src/main/java/com/umbertopalazzini/s3zilla/utility/Consts.java
+++ src/main/java/com/umbertopalazzini/s3zilla/utility/Consts.java
@@ -10,6 +10,11 @@ public class Consts {
     public static final long GB = 1024L * 1024L * 1024L;
     public static final long TB = 1024L * 1024L * 1024L * 1024L;
 
+    // TODO: Replace these three constants with internazionalized strings.
+    public static final String CANCEL = "CANCEL";
+    public static final String PAUSE = "PAUSE";
+    public static final String RESUME = "RESUME";
+
     public static final String ITALIAN = "locales/locale_it_IT";
     public static final String ENGLISH = "locales/locale_en_GB";
 }
